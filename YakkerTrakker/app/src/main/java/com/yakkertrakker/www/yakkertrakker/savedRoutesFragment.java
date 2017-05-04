6a0f6b40cef60f908fdf06a10a61ef55a6da8cec
keegandsouza15 <keegan0415dsouza@gmail.com>
2017-05-03 23:20:40 -0700

    Added saved Routes display, put everything in a relative layout to stop background from causing crashes

--- YakkerTrakker/app/src/main/java/com/yakkertrakker/www/yakkertrakker/savedRoutesFragment.java
+++ YakkerTrakker/app/src/main/java/com/yakkertrakker/www/yakkertrakker/savedRoutesFragment.java
@@ -1,15 +1,21 @@
 package com.yakkertrakker.www.yakkertrakker;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import SQLite.Coordinates;
@@ -26,10 +32,6 @@ public class savedRoutesFragment extends ListFragment {
     }
 
 
-
-    //Array of Strings;
-    String [] testArray = {"Cat", "Dog", "Mouse","Austin"};
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -40,8 +42,7 @@ public class savedRoutesFragment extends ListFragment {
         Routes b = new Routes ("Route2", "04/21/1998","It was a cold day");
         db.addRouteIntoDataBase(a);
         db.addRouteIntoDataBase(b);
-      //  db.deleteRouteFromDataBase(a.getRoute_name());
-      //  db.deleteRouteFromDataBase(b.getRoute_name());
+
 
 
 
@@ -49,40 +50,25 @@ public class savedRoutesFragment extends ListFragment {
 
         int size = rList.size();
         String [] routeNames = new String [size];
-
-        for (int i = 0; i <rList.size(); i++){
+        for (int i = 0; i <rList.size(); i++) {
             Routes temp = rList.get(i);
-            routeNames [i] = temp.getRoute_name();
+            routeNames[i] = temp.getRoute_name() + ":\t " + temp.getDate_created() + "\n\t"+ temp.getComments();
         }
-
-
-
-
-
-
-
 
         View view = inflater.inflate(R.layout.fragment_saved_rotues, container, false);
 
         if (size == 0) {
             String [] noRoutes = {"You currently do not have any saved routes."};
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), R.layout.rowlayout, R.id.textView, noRoutes);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), R.layout.rowlayout, R.id.RouteNametb, noRoutes);
             setListAdapter(adapter);
         }
         else{
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), R.layout.rowlayout, R.id.textView, routeNames);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), R.layout.rowlayout, R.id.RouteNametb, routeNames);
             setListAdapter(adapter);
-
         }
-
-
-
-
-
 
         return view;
     }
-
 
 
 }
